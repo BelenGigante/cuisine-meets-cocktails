62996b48a1a55f1d444330ba0db960c61d8faa17
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2022-12-13 14:04:53 -0500

    added fetch in the if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,14 @@
 var mealUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
 var drinkUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
-
+ 
 $(document).ready(function () {
 
   var displayDrink = function (drink){
     $('#drink-recipe-title').text(drink.strDrink);
     $('#drink-recipe-img').attr('src', drink.strDrinkThumb);
     $('#drink-recipe-p').text(drink.strInstructions);
+    
   }
 
   $(".btn").on("click", function () {
@@ -24,23 +25,35 @@ $(document).ready(function () {
         console.log(error);
       });
 
-    fetch(drinkUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        displayDrink(data.drinks[0]);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    // fetch(drinkUrl)
+    //   .then(function (response) {
+    //     return response.json();
+    //   })
+    //   .then(function (data) {
+    //     console.log(data);
+    //     displayDrink(data.drinks[0]);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
 
     if (document.getElementById('checkbox1').checked) {
+        fetch(drinkUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          displayDrink(data.drinks[0]);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
       console.log("alcoholic");
       document.getElementById('checkbox2').checked = false
     }
     if (document.getElementById('checkbox2').checked) {
+        i
       console.log("no drink");
     }
     if (document.getElementById('checkbox3').checked) {
@@ -50,18 +63,20 @@ $(document).ready(function () {
 
   $(".form-check-input").on("click", function (event) {
     if (event.target.value === 'option1') {
-      console.log("alcoholic");
+      
+        console.log("alcoholic");
       document.getElementById('checkbox2').checked = false
       document.getElementById('checkbox3').checked = false
     }
     if (event.target.value === 'option2') {
       console.log("no drink");
+      return;
       document.getElementById('checkbox1').checked = false
       document.getElementById('checkbox3').checked = false
     }
     if (event.target.value === 'option3') {
       console.log("non alcoholic");
-      document.getElementById('checkbox1').checked = false
+      document.getElementById('checkbox1').checked = true
       document.getElementById('checkbox2').checked = false
     }
 
