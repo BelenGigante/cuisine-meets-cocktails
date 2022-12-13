0b6ce59ea45818d1e9ede37bc7d18456159a9277
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2022-12-13 10:44:03 -0500

    added event listener and jquery link into HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,44 @@ fetch(mealUrl)
   .catch(function (error) {
     console.log(error);
   });
+
+  
+
+$(document).ready(function () {
+
+    $(".btn").on("click", function () {
+        if (document.getElementById('inlineCheckbox1').checked) {
+            console.log("alcoholic");
+            document.getElementById('inlineCheckbox2').checked = false
+        }
+        if (document.getElementById('inlineCheckbox2').checked) {
+            console.log("no drink");
+        }
+        if (document.getElementById('inlineCheckbox3').checked) {
+            console.log("non alcoholic");
+
+        }
+    });
+
+    $(".form-check-input").on("click", function (event) {
+        if (event.target.value === 'option1') {
+            console.log("alcoholic");
+            document.getElementById('inlineCheckbox2').checked = false
+            document.getElementById('inlineCheckbox3').checked = false
+        }
+        if (event.target.value === 'option2') {
+            console.log("alcoholic");
+            document.getElementById('inlineCheckbox1').checked = false
+            document.getElementById('inlineCheckbox3').checked = false
+        }
+        if (event.target.value === 'option3') {
+            console.log("alcoholic");
+            document.getElementById('inlineCheckbox1').checked = false
+            document.getElementById('inlineCheckbox2').checked = false
+        }
+     
+    });
+})
+
+
+
